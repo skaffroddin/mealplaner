344f419bc3affc8599498403db2aaf54965f6168
Sekh Affroddin <87315764+skaffroddin@users.noreply.github.com>
2024-11-08 18:34:53 +0530

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,30 @@
+composer install --no-dev --prefer-dist --optimize-autoloader
+npm install laravel-mix tailwindcss postcss autoprefixer --save-dev
+cp env-final .env
+php artisan key:generate
+npm install
+php artisan config:cache 
+php artisan route:cache 
+php artisan view:cache 
+php artisan serve
+
+
+# cp .env.example .env
+#         php artisan key:generate
+# 2.npm install
+# 3. npx tailwindcss -o public/css/tailwind.css --minify
+# 4. npm run build
+
+
+# cp .env.example .env
+# php artisan key:generate 
+# php artisan config:cache php artisan route:cache php artisan view:cache php artisan serve
+
+
+# php artisan cache:clear && \
+# php artisan route:clear && \
+# php artisan config:clear && \
+# php artisan view:clear && \
+# php artisan optimize:clear && \
+# php artisan config:cache && \
+# php artisan route:cache
